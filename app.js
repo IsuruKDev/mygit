6a50e3d2e187db3540387205240b6dd87ee4c2b4
IsuruK <isurujr.online@gmail.com>
2018-01-10 08:19:17 -0600

    Completed YDKJS

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
 
 
-console.log("auth server : 10.30.3.3");
+// console.log("auth server : 10.30.3.3");
+
+function User() {
+
+    var username;
+    var password;
+
+    function doLogin(uname, pword) {
+        username = uname;
+        password = pword;
+
+        console.log("Username :: "+username);
+        console.log("Password :: "+password);
+    }
+
+    var publicAPI = {
+        login:doLogin
+    }
+
+    return publicAPI;
+}
+
+var fred = User();
+fred.login("IsuruK", "1qaz2wsx@987");
+
+
